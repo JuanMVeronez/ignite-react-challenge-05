1afa43353d76fc2fc02657f4a14d12e1d8969480
Juan Veronez <juan.monteirov@gmail.com>
2021-10-17 16:32:40 -0300

    Estilização do header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,10 @@
 import Link from 'next/link'
 
+import styles from './header.module.scss'
+
 export default function Header() {
   return (
-    <header>
+    <header className={styles.container}>
       <Link href='/'>
         <a>
           <img src="/assets/logo.svg" alt="logo" />
